8538ec8574eb4c51a65e113c7c4339a604b370ad
wmalik90 <98184719+wmalik90@users.noreply.github.com>
2023-04-30 12:29:37 +1200

    comit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Home';
-import ReactGA  from 'react-ga'
-ReactGA.initialize('G-N51B4Y11MN');
+const GA_MEASUREMENT_ID = 'G-N51B4Y11MN';
+
+const sendPageview = () => {
+  const url = `https://www.google-analytics.com/g/collect?v=2&tid=${GA_MEASUREMENT_ID}&cid=${Math.random()}&t=pageview&dp=${window.location.pathname}&ua=navigator.userAgent`;
+  fetch(url, { method: 'POST' });
+};
 
 
 function App() {
-
+  useEffect(() => {
+    sendPageview();
+  }, []);
 
   return (
     <>
