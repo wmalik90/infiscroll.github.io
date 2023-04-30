70323f4e6c932006dfc1de55c16cfc3337d1ff78
wmalik90 <98184719+wmalik90@users.noreply.github.com>
2023-04-30 22:07:51 +1200

    ssd

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Home';
 import ReactGA from 'react-ga';
+import Aboutus from './Aboutus';
 const GA_MEASUREMENT_ID = 'UA-266516599-1';
   ReactGA.initialize(GA_MEASUREMENT_ID);
 
 
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname);
 
+}, []);
 
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<Aboutus />}/>
           {/* <Route path="/about" element={<Aboutus />} />
           <Route path="/login" element={<Login />} /> */}
         </Routes>
--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,11 @@ import React, { useEffect, useState } from 'react'
 import InfiniteScroll from "react-infinite-scroll-component";
 import axios from 'axios'
 import CalendarComponent from './CalenderComponent';
-import { FileLoading } from './FileLoading/FileLoading';
-import ReactGA from 'react-ga';
+// import { FileLoading } from './FileLoading/FileLoading';
+// import ReactGA from 'react-ga';
 
 const Home = () => {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname);
 
-}, []);
   
       /////comm
   const [passengers, setPassengers] = useState([]);
